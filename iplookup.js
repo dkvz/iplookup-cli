b95960a86788f3ebf38edf98dc54b7eefa0f2da6
DkVZ <github@dkvz.eu>
2022-06-02 22:13:25 +0200

    Add flag to only show the country codes (on a single line)

--- iplookup.js
+++ iplookup.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 require('dotenv').config()
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
 const path = require('path')
 const geoip = require('./lib/geoip')
 
@@ -13,14 +15,12 @@ function die(msg) {
   process.exit(1)
 }
 
-async function showIpInfo(ips) {
+async function showIpInfo(ips, short = false) {
   await geoip.openReaders(geoCityPath, geoAsnPath)
   for (ip of ips) {
-    console.log('---')
-    let asTable;
     const city = geoip.getCity(ip)
     const asn = geoip.getASN(ip)
-    console.log(`IP Address: ${ip}`)
+    let asTable
     if (city) {
       asTable = {
         'Country': city.country.names.en,
@@ -37,17 +37,37 @@ async function showIpInfo(ips) {
       asTable['Org'] = asn.autonomousSystemOrganization;
       asTable['AS net'] = asn.network;
     }
-    // Wanted this at the end
+    // At some point I realized I needed this:
     if (city && city.traits && city.traits.network) {
       asTable['IP net'] = city.traits.network
     }
-    console.table(asTable)
+    if (short) {
+      process.stdout.write(`${asTable.ISO ? asTable.ISO : '??'} `)
+    } else {
+      console.log(`IP Address: ${ip}`)
+      console.table(asTable)
+      console.log('---')
+    }
     //console.log(JSON.stringify({ ...city, ...asn }, null, 2))
   }
+  // Just to terminate the line when listing country codes
+  // only:
+  if (short) console.log()
 }
 
-// TODO We could print USAGE if arguments are missing.
-if (process.argv < 3) die('Missing IP address(es) as argument(s)')
-const ipArgs = process.argv.splice(2)
 
-showIpInfo(ipArgs)
+const argv = yargs(hideBin(process.argv))
+  .usage('$0 [-s] IP_ADDRESS1 IP_ADDRESS2 ...')
+  .option('short', {
+    alias: 's',
+    describe: 'Only show the country code',
+    boolean: true,
+    default: false
+  })
+  .help()
+  .parse()
+
+if (argv._.length < 1) die('Missing IP address(es) as argument(s)')
+
+//const ipArgs = process.argv.splice(2)
+showIpInfo(argv._, argv.short)
